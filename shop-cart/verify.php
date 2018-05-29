27833ab76d82b6db7329c6473aad5192a720fe25
boeingmagr <201601061@iacademy.edu.ph>
2018-05-29 12:21:07 +0800

    login
    added verify.php
    deleted login.php
    
    Co-Authored-By: kiararose <kiararose@users.noreply.github.com>
    Co-Authored-By: sheila valdez <sheila004@users.noreply.github.com>
    Co-Authored-By: robabyko <robabyko@users.noreply.github.com>

---
+++ shop-cart/verify.php
@@ -0,0 +1,39 @@
+<?php
+  $db = mysqli_connect('localhost','root','') or die("cannot connect");
+  mysqli_select_db($db,"grocery2go") or die("cannot select DB ");
+
+
+  if(isset($_POST['email']) and isset($_POST['password'])) {
+      // username and password sent from form
+
+      $email = mysqli_real_escape_string($db,$_POST['email']);
+      $password = mysqli_real_escape_string($db,$_POST['password']);
+
+      echo $email;
+      echo $password;
+
+      $sql = "SELECT * FROM customer_details WHERE email = '$email' and password = '$password'";
+      $result = mysqli_query($db,$sql) or die(mysqli_error($db));
+      $row = mysqli_fetch_array($result);
+      $firstName = $row['firstname'];
+      $username = $row['username'];
+      $count = mysqli_num_rows($result);
+
+      echo $count;
+
+
+      // If result matched $myusername and $mypassword, table row must be 1 row
+
+      if($count == 1) {
+         //session_register("myusername");
+         session_start();
+         $_SESSION['firstname'] = $firstName;
+         $_SESSION['username'] = $username;
+         echo "redirect me";
+         header('Location:home.php');
+      }else {
+         header("Location:index.php?loginFailed=true&reason=password");
+      }
+   }
+
+?>
