e34dcc86a24b6c3afd1f481bb1ca75683553019b
kiararose <31794881+kiararose@users.noreply.github.com>
2018-05-29 14:36:01 +0800

    :))))))
    kia

--- shop-cart/dairy-list.php
+++ shop-cart/dairy-list.php
@@ -7,7 +7,7 @@
     $product_array = $shoppingCart->getAllProduct($query);
     if (! empty($product_array)) {
         foreach ($product_array as $key => $value) {
-          if ($product_array[$key]["categ_id"] == 1) {
+          if ($product_array[$key]["categ_id"] == 13) {
             // code...
 
             ?>
