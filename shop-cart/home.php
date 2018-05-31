8c7fd340efad90639faf4878e95f93b87de4a6e9
boeingmagr <201601061@iacademy.edu.ph>
2018-05-31 18:49:53 +0800

    livetracking
    livetracking

--- shop-cart/home.php
+++ shop-cart/home.php
@@ -59,7 +59,7 @@ header ('Location:verify.php');
         <li class="active"><a href="home.php">Home</a></li>
         <li class=""><a href="freshmeat.php">ShopNow</a></li>
         <li class=""><a href="faqs.php">FAQs</a></li>
-        <li class=""><a href="">Live Track</a></li>
+        <li class=""><a href="livetrack.html">Live Track</a></li>
           <li class=""><a href="">Message</a></li>
           <li class=""><a href="profile.php">Profile</a></li>
       </ul>
